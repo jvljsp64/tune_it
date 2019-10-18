e58e5a6fb750e60268e3ea9e6a52bb31e4bb662f
JovalJoseph <56705989+JovalJoseph@users.noreply.github.com>
2019-10-18 13:27:55 +0530

    Update tune_it.sh

--- tune_it.sh
+++ tune_it.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#sudo apt update
+sudo apt install git john apktool dosbox apache2 numlockx wget unzip steghide vbindiff binwalk terminator nasm git python-pip secure-delete wireshark exiftool ghex libc-dev gcc-multilib gem ruby -y
+sudo gem install zsteg 
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
+pip install pwntools pycrypto sympy
+sudo apt-get install python2.7-gmpy* && sudo apt-get install python3-gmpy*
+git clone https://github.com/radare/radare2
+cd radare2
+sudo sys/install.sh
